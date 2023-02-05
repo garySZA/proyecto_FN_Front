bbcaff949d318bf41c3646e3879ad6963cc58f5e
GarySZ <garysoliz07@gmail.com>
2023-02-05 14:53:18 -0400

    form control in login added

---
+++ src/components/input/Input.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Controller, useFormState } from 'react-hook-form'
+
+const styleError = {
+    
+}
+
+export const Input = ({ name, type, placeholder, label }) => {
+    const formState = useFormState();
+
+    return (
+        <Controller
+            name={ name }
+            render={({field}) => (
+                <div className='mb-3'>
+                    <label htmlFor="email" className='form-label'>{ label }</label>
+                    <input 
+                        type={ type } 
+                        placeholder={ placeholder }
+                        className='form-control'
+                        { ...field }
+                    />
+                    { 
+                        formState.errors[name] &&
+                            <small
+                                className='text-danger'
+                            >
+                                { String(formState.errors[name] ? formState.errors[name]?.message : '' )}
+                            </small>
+                    }
+                </div>
+            )}
+        >
+
+        </Controller>
+    )
+}
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,30 +1,52 @@
-import React, { useContext } from 'react'
-import { useForm } from 'react-hook-form';
+import React, { useContext, useState } from 'react'
+import { useForm, FormProvider } from 'react-hook-form';
 import { NavLink, useNavigate } from 'react-router-dom'
+import { yupResolver } from '@hookform/resolvers/yup';
+
+import * as yup from 'yup';
+
 import { StateContext } from '../context/stateProvider';
 import { validarEmail } from '../helpers/validators';
+import { AuthContext } from '../context/AuthContext.jsx';
+import { Input } from '../components/input/Input';
 
-const listClients = [
-    {
-        id: 1,
-        name: 'juan'
-    },
-    {
-        id: 2,
-        name: 'Gary'
-    }
-]
+const schema = yup.object().shape({
+    email: yup.string()
+        .email('El email ingresado no es válido')
+        .required('El email es requerido'),
+
+    password: yup.string()
+        .min(8, 'La contraseña debe tener almenos 8 caracteres')
+        .required('La contraseña es requerida')
+});
+
+const defaultValues = {
+    email: '',
+    password: ''
+};
 
 export const Login = () => {
     const navigate = useNavigate();
+    const auth = useContext(AuthContext);
+    const form = useForm({
+        resolver: yupResolver( schema ),
+        defaultValues: defaultValues,
+    });
+
     const { state, dispatch } = useContext(StateContext);
 
     const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const [showPassword, setShowPassword] = useState(false);
     
     const onSubmit = ( data ) => {
         console.log(data)
         dispatch({ type: 'setClients', payload: listClients })
         state.listClients
+    }
+
+    const onError = ( error ) => {
+        console.log('error', error);
     }
 
     const handleOnBack = () => {
@@ -40,25 +62,31 @@ export const Login = () => {
                     >
                         Iniciar Sesión
                     </h2>
-                    <form onSubmit={ handleSubmit( onSubmit ) }>
-                        <div className="mb-3">
-                            <label for="exampleFormControlInput1" className="form-label">Email:</label>
-                            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-                        </div>
-                        <div className="mb-2">
-                            <label for="exampleFormControlInput1" className="form-label">Contraseña:</label>
-                            <input type="password" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-                        </div>
-                        <a className='text-titles' href='/new_account'>He olvidado mi contraseña</a>
-                        <div className='d-flex justify-content-center'>
-                            <input 
-                                type="submit" 
-                                value='Acceder'
-                                className='btn btn-secondary w-75 rounded-pill mt-3'
+                    <FormProvider {...form}>
+                        <form onSubmit={ form.handleSubmit(onSubmit, onError) }>
+                            <Input 
+                                name='email'
+                                type='email'
+                                placeholder='tuemail@ejemplo.com'
+                                label='Email'
                             />
+                            <Input 
+                                name='password'
+                                type='password'
+                                placeholder='tu contraseña'
+                                label='Contraseña'
+                            />
+                            <small className='text-titles'>He olvidado mi contraseña</small>
+                            <div className='d-flex justify-content-center'>
+                                <input 
+                                    type="submit" 
+                                    value='Acceder'
+                                    className='btn btn-secondary w-75 rounded-pill mt-3'
+                                />
 
-                        </div>
-                    </form>
+                            </div>
+                        </form>
+                    </FormProvider>
                     <div className='d-flex justify-content-center'>
                         <button
                             className='btn w-75 mb-2'
@@ -67,11 +95,11 @@ export const Login = () => {
                             Volver
                         </button>
                     </div>
-                    <p
-                        className='text-titles fs-6'
+                    <small
+                        className='text-titles'
                     >
                         Aún no tienes una cuenta? <NavLink className='text-secondary' to={'/new_account'}>Crear cuenta aquí</NavLink>
-                    </p>
+                    </small>
                 </div>
             </div>
         </div>
