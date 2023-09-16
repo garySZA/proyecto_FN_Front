7a0d87222871b560a0c020114b1f5a1ebfe55108
GarySZ <garysoliz07@gmail.com>
2023-09-15 21:26:23 -0400

    BUG - problemas con libs como react-icons

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -3,8 +3,9 @@ import * as ReactIcons from 'react-icons/all'
 import { FaUser } from 'react-icons/fa'
 
 export const Icon = ({ icon, title = 'Usuarios', size = 25, color = 'primary', className='mt-1' }) => {
-    if( ReactIcons[icon] ){
-        const IconComponent = ReactIcons[icon];
+    const IconComponent = ReactIcons[icon];
+    
+    if( IconComponent ){
 
         return <IconComponent className={`text-${color} ${ className }`} title={ title } size={ size }  />
     } else {
